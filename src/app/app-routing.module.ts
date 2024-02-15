18c62bdb9c25df0c81b611dfee24ffb72acfe0cd
Francisc Szasz <101558212+franc51@users.noreply.github.com>
2024-02-15 13:38:52 +0200

    started routernav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterVehicleComponent } from './containers/register-vehicle/register-vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register-vehicle', component: RegisterVehicleComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
